3629978c4217a2ee3381d7dca6640ea5983f57c2
imcnr <imcnair13@gmail.com>
2019-07-17 13:42:00 -0400

    Tried using setup.py but it doesn't work

--- setup.py
+++ setup.py
@@ -8,8 +8,14 @@ executables = [cx_Freeze.Executable("ParticleFilter.py")]
 
 cx_Freeze.setup(
     name="Particle Filter Simulation",
-    options={"build_exe": {"packages":["pygame", "random","math"],
-                           "include_files":["racecar.png"]}},
+    options={"build_exe": {"packages":["pygame", 
+                                       "random", 
+                                       "math"],
+                           "include_files":["cat_roomba2.png",
+                                            "draw_functions.py",
+                                            "Event_and_Movement.py",
+                                            "landmarks.py",
+                                            "Particle.py",
+                                            "settings.py"]}},
     executables = executables
-
     )
