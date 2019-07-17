119fe55f804563d976c8d4ed18845a3ec108e2c3
imcnr <imcnair13@gmail.com>
2019-07-17 11:58:00 -0400

    changed settings

--- settings.py
+++ settings.py
@@ -4,8 +4,8 @@ class Settings:
         Can be changed to see how randomness affects the performance
     """
     def __init__(self):
-        self.display_width = 1000 # Should be int
-        self.display_height = 800 # Should be int
+        self.display_width = 700 # Should be int
+        self.display_height = 500 # Should be int
         self.displacement = 10.0 # Starting Speed
         self.num_particles = 1000
         self.forward_variance = 5.
